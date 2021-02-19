c50a5051795ad3c6d69a77af0a825e9dc860151a
Pitiot Alexandre <alexandre.pitiot01@gmail.com>
2021-02-19 11:40:49 +0100

    evalutation javascript

--- eval JAVASCRIPT/exo4.js
+++ eval JAVASCRIPT/exo4.js
@@ -10,23 +10,23 @@ if (tot>=100 && tot<=200) {
     rem2=("5%")
     tot= tot-rem; // Si le total est compris entre 100 et 200 alors la remise sera de 5 %, 
 }    
-    else if (tot>200){
+    else if (tot>200){ // si le total est superieur à 200 alors la remise sera de 10 %
         rem=(tot*10)/100;
         rem2=("10%")
         tot = tot-rem;
     }
-    else{
+    else{ // sinon il n'y aura pas de remise
         rem=0
     }
-if (tot>300 && tot<500){
+if (tot>300 && tot<500){ // si le totale de la commande comprenant la remise est compris entre 300 et 500, alors les fdp s'éleveront à 2% du su total de la commande
     port=(tot*2)/100
     tot= tot+port
 }
-    else if (tot>500) {
+    else if (tot>500) { //si le totale de la commande comprenant la remise est superieur à 500, alors les fdp seront offert
         port=0
         tot = tot+port
     }
-    else {
+    else { // sinon les fdp s'éleveront à 6 euros
         port=6
         tot= tot+port
     }
