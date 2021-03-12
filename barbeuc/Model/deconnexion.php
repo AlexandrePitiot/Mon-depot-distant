5cc3ef956ab8f2f12872f18c8f84195343097006
Pitiot Alexandre <alexandre.pitiot01@gmail.com>
2021-03-12 10:18:21 +0100

    fin tba

---
+++ barbeuc/Model/deconnexion.php
@@ -0,0 +1,16 @@
+<?php
+
+// On détruit la session avec session_destroy()
+
+session_start();
+if(isset($_GET['logout']))
+{
+    //session_unset()
+    session_destroy();
+    header("location:../index.php");
+}
+else{
+    header("location:../index.php");
+}
+
+?>
