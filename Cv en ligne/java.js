4ac466a398c461f92e4c3426730e1df6e8530b4b
Pitiot Alexandre <alexandrepitiot01@gmail.com>
2021-04-28 16:14:10 +0200

    cv en ligne

---
+++ Cv en ligne/java.js
@@ -0,0 +1,8 @@
+function toggle_text(id) {
+    let span = document.getElementById(id);
+    if(span.style.display === "none") {
+        span.style.display = "inline";
+    } else {
+        span.style.display = "none";
+    }
+}
